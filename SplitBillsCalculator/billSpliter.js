0a49c82c7643dab56223ea40e2e4fa5ef61b8813
Lucca Augusto Torres Tambor <lucca.tambor@unesp.br>
2022-01-16 17:03:35 -0300

    implemented the calcultation of each payment

--- SplitBillsCalculator/billSpliter.js
+++ SplitBillsCalculator/billSpliter.js
@@ -1,3 +1,5 @@
+var clients;
+
 function change(str) {
     var b = document.querySelector('#count');
     if (str === 'u') {
@@ -9,9 +11,11 @@ function change(str) {
 }
 
 function createCalc() {
-    console.log('começando');
+    document.querySelector('#btn-start').disabled = true;
     var num = document.querySelector('#count').value;
+    clients = num;
     let grid = document.querySelector('#calcGrid');
+    
     var i;
     
     for (i = 1; i <= num; i++) {
@@ -25,8 +29,6 @@ function createCalc() {
         personTitle.placeholder = 'Nome do cliente';
         person.appendChild(personTitle);
 
-        
-
         let itemsDiv = document.createElement('div');
         itemsDiv.id = 'items_div_'+ person.id;
         console.log(itemsDiv.id);
@@ -39,20 +41,71 @@ function createCalc() {
         person.appendChild(addItemBtn);
         addItemBtn.addEventListener('click', function() { createNewItem(itemsDiv)});
 
+        let taxCheck = document.createElement('input');
+        taxCheck.setAttribute('type', 'checkbox');
+        taxCheck.setAttribute('class', 'check_tax');
+        taxCheck.setAttribute('id', 'checkTax_'+person.id);
+        console.log(taxCheck.id);
+        taxCheck.innerHTML = "10%";
+
+        var br = document.createElement("br");
+        
+        let label_check = document.createElement('label');
+        label_check.innerHTML = "Taxa de Serviço";
+
+        person.appendChild(br);
+        person.appendChild(taxCheck);
+        person.appendChild(label_check);
+
+        let finalValue = document.createElement("h3");
+        finalValue.setAttribute('id', 'final_value_'+person.id);
+        finalValue.setAttribute('class', 'final_value');
+        finalValue.innerHTML = "R$0.00";
+
+        person.appendChild(finalValue);
+
         grid.appendChild(person);
     }
+
+    let btnExec = document.createElement('button');
+    btnExec.innerHTML = "Calcular";
+    btnExec.setAttribute('id', 'btn_exec');
+    btnExec.setAttribute('class', 'btn-count');
+    btnExec.addEventListener('click', doCalc);
+
+    grid.appendChild(btnExec);
 }
 
 function createNewItem (div) {
-    var items_number=1;
     let newItem = document.createElement('input');
     newItem.placeholder = 'Nome do item';
     div.appendChild(newItem);
 
     let newItemPrice = document.createElement('input');
     newItemPrice.placeholder = 'Preço do item';
+    newItemPrice.setAttribute('class', 'price_text_' + div.id);
     newItemPrice.type = 'number';
     newItemPrice.step = '0.01';
+    console.log(newItemPrice.className);
     div.appendChild(newItemPrice);
-    
 }
+
+function doCalc () {
+    var num = clients, 
+        i,j;
+    for (i = 1; i <= num; i++) {
+        let str = 'person_' + i;
+        let itemsList = document.querySelectorAll('input.price_text_items_div_'+str);
+        let sum = 0.00;
+        
+        for(j = 0; j < itemsList.length; j++) {
+            sum += parseFloat(itemsList[j].value);
+        }
+
+        if(document.querySelector('#checkTax_person_'+i).checked == true) {
+            sum = sum * 1.1;
+        }
+
+        document.querySelector('#final_value_person_'+i).innerHTML = "R$ "+ sum.toFixed(2);
+    }
+ }
